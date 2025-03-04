f7d744c790ef071d02c2e8f64b859876d8683550
michael-k <michaelsure9@gmail.com>
2025-03-04 11:51:23 +0200

    starting

--- lib/mqtt_node.dart
+++ lib/mqtt_node.dart
@@ -1,48 +1,51 @@
 import 'dart:async';
 import 'dart:io';
 
+Timer? backgroundTask;
+bool isRunning = false;
+
 bool run() {
 
-  Timer? backgroundTask;
-  bool isRunning = false;
+  print('Enter command (s[tart]/f[inal]/e|x[xit]):');
 
-  print('Enter command (s[tart]/f[inal]/e[xit]):');
-
+  doneCommand('s');
 
   stdin.listen((List<int> data) {
     String command = String.fromCharCodes(data).trim();
-
-    if (command == 's') {
-      if (!isRunning) {
-        isRunning = true;
-        print('Task is started');
-        backgroundTask = Timer.periodic(Duration(seconds: 2), (timer) {
-          if (!isRunning) {
-            timer.cancel();
-            return;
-          }
-          //print('Task in progress...');
-        });
-      } else {
-        print('Task already in progress...');
-      }
-    } else if (command == 'f') {
-      if (isRunning) {
-        isRunning = false;
-        print('Task stopped');
-      } else {
-        print("Task isn't active");
-      }
-    }
-    else if (command == 'e') {
-      print("App exit");
-      exit(0);
-    }
-    else {
-      print('Invalid command "$command", use start или stop.');
-    }
+    doneCommand(command);
   });
-
-
   return true;
 }
+
+void doneCommand(String command) {
+  if (command == 's') {
+    if (!isRunning) {
+      isRunning = true;
+      print('Task is started');
+      backgroundTask = Timer.periodic(Duration(seconds: 2), (timer) {
+        if (!isRunning) {
+          timer.cancel();
+          return;
+        }
+        //print('Task in progress...');
+      });
+    } else {
+      print('Task already in progress...');
+    }
+  } else if (command == 'f') {
+    if (isRunning) {
+      isRunning = false;
+      print('Task stopped');
+    } else {
+      print("Task isn't active");
+    }
+  }
+  else if (command == 'e' || command == 'x') {
+    print("App exit");
+    exit(0);
+  }
+  else {
+    print('Invalid command "$command", use start или stop.');
+  }
+}
+

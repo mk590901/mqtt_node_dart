ce90f58e934150a9cfdc6b1414ad98f2e762526e
michael-k <michaelsure9@gmail.com>
2025-03-06 12:57:40 +0200

    procedure messagesAssembly was changed.

---
+++ lib/slicer/files_container.dart
@@ -0,0 +1,15 @@
+import 'file_chunks.dart';
+
+class FilesContainer {
+  final Map<String,FileChunks> _container = {};
+
+  void put (final String file, FileChunks chunks) {
+    _container[file] = chunks;
+  }
+
+  FileChunks? get (final String file) {
+    return _container[file];
+  }
+
+
+}
